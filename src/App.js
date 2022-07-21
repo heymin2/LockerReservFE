b43a756c0ecc5148108aed494caab0941a1a057a
alddl <131025_@naver.com>
2022-07-21 23:47:16 +0900

    [추가] 로그인 화면

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Login.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        프로젝트 생성 후 수정 확인
-      </header>
-    </div>
+    <Login />
   );
-}
+};
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import './Login.scss';
+import axios from 'axios';
+
+const Login = ({ login }) => {
+    return (<div>
+        <div className="rect" />
+        <div className="word">사물함 로그인</div>
+        <form>
+            <input className="num" placeholder='학번 ex)22121234' />
+            <input type='text' className="name" placeholder='이름 ex)김컴공' />
+            <input className="phoneNum" placeholder='전화번호 ex)01012345078' />
+            <div className="PIC" />
+            <button classname="log">로그인</button>
+        </form>
+    </div>
+    );
+};
+
+export default Login;
