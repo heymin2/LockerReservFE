932a48f2fdd5c07f11618f998425fd450b08e0ea
alddl <131025_@naver.com>
2022-07-22 00:05:52 +0900

    [추가] 로그인 화면

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,7 @@ const Login = ({ login }) => {
             <input type='text' className="name" placeholder='이름 ex)김컴공' />
             <input className="phoneNum" placeholder='전화번호 ex)01012345078' />
             <div className="PIC" />
-            <button classname="log">로그인</button>
+            <button className="logbtn">로그인</button>
         </form>
     </div>
     );
