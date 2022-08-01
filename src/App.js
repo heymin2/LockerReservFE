703c77dc699fd4748168c64809a19b4d4c1cb8d2
alddl <131025_@naver.com>
2022-08-02 00:15:57 +0900

    [수정] 모달창 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { useState } from 'react';
-import Modal from './Modal';
-import Login from './Login.js';
-import PIC from './PIC';
-import LoginCheck from './LoginCheck';
+import SignButton from './layouts/SignButton';
 
 function App() {
-  let [modal, setModal] = useState(false);
 
   return (
     <div>
-      <button className='btn' onClick={() => setModal(!modal)}>로그인</button>
-      {modal === true ? <Modal /> : null}
-      {/* <Login /> */}
-      <LoginCheck />
+      <SignButton />
     </div>
   );
 };
