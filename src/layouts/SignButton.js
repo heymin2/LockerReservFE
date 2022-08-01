9a80cfbf492b6d0078dddd2df3863b6893a79b94
alddl <131025_@naver.com>
2022-08-02 00:19:16 +0900

    [수정] 모달창

---
+++ src/layouts/SignButton.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import SignUpModal from '../modals/SignUpModal';
+import Login from '../Login.scss';
+import { Button } from "react-bootstrap";
+
+const SignButton = () => {
+    const [signUpModalOn, setSignUpModalOn] = useState(false);
+    return (
+        <>
+            <SignUpModal
+                show={signUpModalOn}
+                onHide={() => setSignUpModalOn(false)}
+            />
+            <Button onClick={() => setSignUpModalOn(true)}>로그인</Button>
+        </>
+    )
+}
+
+export default SignButton
---
+++ src/modals/SignUpModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import PIC from '../PIC';
+
+const SignUpModal = ({ show, onHide }) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <p className="word">사물함 로그인</p>
+            <form>
+                <input type="text" name="Num" className="num" placeholder='학번 ex)22121234' minLength="8" maxLength="8" />
+                <input type="text" name="Name" className="name" placeholder='이름 ex)김컴공' minLength="4" />
+                <input type="tel" name="phoneNum" className="phoneNum" placeholder='전화번호 ex)01012345078' minLength="11" maxLength="11" />
+                <PIC />
+                <input type="submit" className="logbtn"></input>
+            </form>
+            <Modal.Footer>
+                <Button onClick={onHide}>닫기</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default SignUpModal
