e1be9a16b28e89c364007252dd45ffcf0ba12fa9
alddl <131025_@naver.com>
2022-08-02 22:51:06 +0900

    [추가] input 미입력시 submit 불가능

--- src/modals/CancelModal.js
+++ src/modals/CancelModal.js
@@ -17,7 +17,7 @@ const CancelModal = ({ show, onHide }) => {
                     <p className="word3">사물함 예약 취소</p>
                     <form>
                         <p className="res">선택한 사물함</p>
-                        <input type="text" className="pass" placeholder='비밀번호 4자리' minLength="4" maxLength="4" />
+                        <input type="text" className="pass" placeholder='비밀번호 4자리' minLength="4" maxLength="4" required />
                         <p className='hint2'><b>사물함 변경하고 싶은데<br /> 비밀번호를 잊으신 분은<br />1:1문의해 주세요</b></p>
                         <input type="submit" className="canbtn" value="취소"></input>
                     </form>
--- src/modals/ReservModal.js
+++ src/modals/ReservModal.js
@@ -17,8 +17,8 @@ const SignUpModal = ({ show, onHide }) => {
                     <p className="word2">사물함 예약</p>
                     <form>
                         <p className="res">선택한 사물함</p>
-                        <input type="text" className="pass" placeholder='비밀번호 4자리' minLength="4" maxLength="4" />
-                        <input type="text" className="num2" placeholder='학번 ex)22121234' minLength="8" maxLength="8" />
+                        <input type="text" className="pass" placeholder='비밀번호 4자리' minLength="4" maxLength="4" required />
+                        <input type="text" className="num2" placeholder='학번 ex)22121234' minLength="8" maxLength="8" required />
                         <p className='hint'><b>사물함 예약 변경하기 위해서<br /> 비밀번호가 꼭 필요하니<br />기억할 수 있는 번호로<br /> 설정해 주세요</b></p>
                         <input type="submit" className="resbtn" value="예약"></input>
                     </form>
