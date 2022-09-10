56e4e864f283292155b642987eb0298146104ddc
alddl <131025_@naver.com>
2022-09-10 19:18:20 +0900

    [수정] 사물함 색 이름 수정

--- src/mainpage/In.js
+++ src/mainpage/In.js
@@ -3,30 +3,24 @@ import '../css/Right.css';
 import axios from 'axios';
 
 const In = () => {
-    const logout = {
-        logout: logout
-    };
-
-    const OnclickLogout = () => {
-        axios.post('http://13.125.255.247:5000/login/logout', logout)
+    const onClickLogout = () => {
+        axios.post('http://13.125.255.247:5000/login/logout', { logout: "logout" })
             .then((res) => {
                 console.log(res);
                 if (res === true) {
                     window.location = '/';
                 }
             });
-    };
+    }
 
     return (
         <aside className="asideright">
             <div className='profilebox'>
                 <img src='프로필사진.png' className='profile' alt="profile"></img>
-                <div className='txt'>
-                    사물함<br /> 예약하지 않음
-                </div>
-                <button className='logout' value='logout' onClick={OnclickLogout}>로그아웃</button>
+                <div className='txt'>사물함<br /> 예약하지 않음</div>
+                <button className='logout' onClick={onClickLogout}>로그아웃</button>
             </div>
-        </aside>
+        </aside >
     );
 }
 
